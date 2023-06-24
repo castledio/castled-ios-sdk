ea457dcb778d4e3591b5bdb128e44859423d80a0
Antony Joe Mathew <antonyjoemathew@gmail.com>
2023-06-24 21:18:55 +0530

    SPM example added

---
+++ Example/CastledDemoSPM/CNotificationContentDemo/NotificationViewController.swift
@@ -0,0 +1,22 @@
+//
+//  NotificationViewController.swift
+//  CNotificationContentDemo
+//
+//  Created by antony on 24/06/2023.
+//
+
+import UIKit
+import UserNotifications
+import UserNotificationsUI
+import CastledNotificationContent
+
+class NotificationViewController: CastledNotificationViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    override func didReceive(_ notification: UNNotification) {
+        super.didReceive(notification)
+    }
+
+}
---
+++ Example/CastledDemoSPM/CNotificationServiceDemo/NotificationService.swift
@@ -0,0 +1,29 @@
+//
+//  NotificationService.swift
+//  CNotificationServiceDemo
+//
+//  Created by antony on 24/06/2023.
+//
+
+import UserNotifications
+import CastledNotificationService
+
+class NotificationService: CastledNotificationServiceExtension {
+
+    var contentHandler: ((UNNotificationContent) -> Void)?
+    var bestAttemptContent: UNMutableNotificationContent?
+
+    override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
+
+        super.didReceive(request, withContentHandler: contentHandler)
+    }
+    
+    override func serviceExtensionTimeWillExpire() {
+        // Called just before the extension will be terminated by the system.
+        // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
+        if let contentHandler = contentHandler, let bestAttemptContent =  bestAttemptContent {
+            contentHandler(bestAttemptContent)
+        }
+    }
+
+}
