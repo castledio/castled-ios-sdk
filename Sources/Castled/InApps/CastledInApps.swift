d8a5077ebb1f01268d3f3d6c9d124cd04f5b2805
Antony Joe Mathew <antonyjoemathew@gmail.com>
2024-03-15 15:13:42 +0530

    cleanups

--- Sources/Castled/InApps/CastledInApps.swift
+++ Sources/Castled/InApps/CastledInApps.swift
@@ -129,7 +129,7 @@ import UIKit
         params?[CastledConstants.PushNotification.CustomProperties.Category.Action.clickAction] = clickAction
         params?[CastledConstants.PushNotification.CustomProperties.Category.Action.buttonTitle] = buttonTitle
 
-        var clickActionType = clickAction.getCastledClickActionType()
+        let clickActionType = clickAction.getCastledClickActionType()
         CastledButtonActionHandler.notificationClicked(withNotificationType: .inapp, action: clickActionType, kvPairs: params, userInfo: params ?? [String: String]())
     }
 
