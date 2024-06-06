24d6aeef64576ef16e33dec82823ccf388eadfc0
Antony Joe Mathew <antonyjoemathew@gmail.com>
2024-06-06 10:42:39 +0530

    network handling for 400 series

--- Sources/Castled/Networking/Core/CastledNetworkLayer.swift
+++ Sources/Castled/Networking/Core/CastledNetworkLayer.swift
@@ -47,12 +47,18 @@ public class CastledNetworkLayer: NSObject {
                         return CastledResponse<T>(response: ["success": "1"] as! T)
 
                     } else {
+                        var statusCode = 0
+                        var errorMsg = CastledExceptionMessages.common.rawValue
+                        if let httpResponse = response as? HTTPURLResponse, !shouldDecodeResponse {
+                            statusCode = httpResponse.statusCode
+                            errorMsg = "Invalid input params"
+                        }
                         if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
                             if let error_message = json["message"] as? String {
-                                return CastledResponse<T>(error: error_message, statusCode: 0)
+                                return CastledResponse<T>(error: "\(error_message) \(String(describing: request.parameters))", statusCode: statusCode)
                             }
                         }
-                        return CastledResponse<T>(error: CastledExceptionMessages.common.rawValue, statusCode: 0)
+                        return CastledResponse<T>(error: "\(errorMsg) \(String(describing: request.parameters))", statusCode: statusCode)
                     }
                 } else {
                     // Fallback on earlier versions
@@ -70,8 +76,21 @@ public class CastledNetworkLayer: NSObject {
     public func makeApiCall<T: Codable>(request: CastledNetworkRequest, path: String, responseModel: T.Type = [String: String].self, shouldDecodeResponse: Bool = false, withRetry: Bool = false, completion: @escaping (_ response: CastledResponse<T>) -> Void) {
         Task {
             let api_response = await CastledNetworkLayer.shared.sendRequestWith(request: request, path: path, responseModel: responseModel, shouldDecodeResponse: shouldDecodeResponse)
-            if !api_response.success && withRetry {
-                CastledStore.enqueFailedRequest(request)
+            if !api_response.success, !shouldDecodeResponse {
+                if (400 ... 499).contains(api_response.statusCode) {
+                    // ignoring the result if its 400 series
+                    if withRetry {
+                        completion(api_response)
+                    } else {
+                        // retuens success to remove from the queue if any for 400 series
+                        completion(CastledResponse<T>(response: ["success": "1"] as! T))
+                    }
+                    return
+                }
+                if withRetry {
+                    // adding the requests to the queue for other errors
+                    CastledStore.enqueFailedRequest(request)
+                }
             }
             completion(api_response)
         }
